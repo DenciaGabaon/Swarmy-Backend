f25048665f571b91a26e575357da3b70e4aa3d7c
matthewanavale <navalematthew12@gmail.com>
2024-05-22 23:25:34 +0800

    scheduling incomplete and conflicts

---
+++ check_conflict.py
@@ -0,0 +1,38 @@
+import json
+
+def check_conflicts(data):
+  """
+  Checks for conflicting schedules in the given data.
+
+  Args:
+    data: A list of tuples, where each tuple represents a class with the following format:
+      (section, course, professor, day,  room)
+
+  Returns:
+    A list of tuples containing conflicting classes (section, course, professor)
+  """
+  conflicts = []
+  for i, class1 in enumerate(data):
+    for j, class2 in enumerate(data):
+      if i != j and ((class1[2] == class2[2] and class1[3] == class2[3]) or (
+              class1[3] == class2[3] and class1[4] == class2[4])):
+        print(f"Conflict found between {class1[0]} {class1[1]} and {class2[0]} {class2[1]}")
+        conflicts.append((class1[0], class1[1], class1[2], class1[3], class1[4]))
+        conflicts.append((class2[0], class2[1], class2[2], class2[3], class2[4]))
+  return conflicts
+
+def main():
+  # Load data from JSON file
+  with open('schedule.json', 'r') as f:
+      json_data = json.load(f)
+  data = []
+
+  for section, entries in json_data.items():
+      for entry in entries:
+          data.append(tuple(entry))
+  print(data[1])
+  check_conflicts(data)
+
+
+if __name__ == '__main__':
+  main()
